ee9996b9be1c1becf75435f0b960a6d9166d7eac
Baymax_Chuang <350535629@qq.com>
2017-12-04 05:05:45 -0800

    Change the format

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What\'s your name', validators=[Required()])
+    submit = SubmitField('Submit')
+
--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from flask import render_template, session, redirect, url_for
 from . import main
 from .forms import NameForm
 
-@main.route('/', methos=['GET','POST'])
+@main.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
