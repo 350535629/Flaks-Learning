8f8c3ef62396eccf58a3095c7603a3e485733d7d
IceBell <350535629@qq.com>
2018-11-18 11:51:37 -0800

    Abort, redirect, user_agent, 400

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python3
 
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1> Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1> Hello, %s!!</h1>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hello, %s!!</h1>' % name
+    if name == 'ice':
+        abort(404)
+    return '<h1> Hello, user %s!</h1>' % name
+
+@app.route('/user/noname')
+def noname():
+    return '<h1> Sorry</h1>', 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
